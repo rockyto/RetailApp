7a0fc7c6fabf20fba7018ad730c9f7810a5e19ff
Rodrigo Sánchez <oc.rodrigosanchez@gmail.com>
2022-11-08 11:36:10 -0600

    Initial Commit

---
+++ Retail/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Retail
+//
+//  Created by Rodrigo Sánchez on 08/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
